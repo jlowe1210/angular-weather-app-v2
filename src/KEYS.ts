f344852da86d1bcd8fff3701e74e6a1d6628da9f
Justin Lowe <justin.lowe53@yahoo.com>
2023-08-29 00:20:41 -0400

    prep for github pages

---
+++ src/KEYS.ts
@@ -0,0 +1,7 @@
+const keys = {
+  accessToken:
+    'pk.eyJ1Ijoiamxvd2UxMjEwIiwiYSI6ImNsZGJheG14dTByNnMzdm5uNmY2dWVjdWYifQ.fTdpaOtdQ4noHCHnEQ7Cmg',
+  openweathermapid: '8c4711fa26eb45ad7a7d5cee8b1ca1d2',
+};
+
+export default keys;
